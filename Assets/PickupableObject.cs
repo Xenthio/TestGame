28ea2ab3d0494b629420902a2d1ddac462253f6b
Xenthio <thegooster88@gmail.com>
2020-08-12 01:26:46 +1000

    fake hud until i add functionality

--- Assets/PickupableObject.cs
+++ Assets/PickupableObject.cs
@@ -9,6 +9,7 @@ public class PickupableObject : MonoBehaviour {
     FirstPersonAIO playerController;
    	GameObject holdPos;
    	Collider plyr; 
+   	FixedJoint joint;
 
         
     float throwPower = 10f;             //amount of throw power
@@ -16,9 +17,7 @@ public class PickupableObject : MonoBehaviour {
 
     private void Awake()
     {
-    	holdPos = new GameObject("HoldPosition");
-    	holdPos.transform.SetParent(Camera.main.transform);                                              //turns Kinematic on, so object will not interfere when picked up
-	    holdPos.transform.localPosition = new Vector3(0f, 0f, 1.5f);
+    	holdPos = GameObject.Find("HoldPosition");
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         rb = transform.GetComponent<Rigidbody>();
         playerController = GameObject.Find("Player").GetComponent<FirstPersonAIO>();
@@ -28,44 +27,53 @@ public class PickupableObject : MonoBehaviour {
     private void Update()
     {
         HandleObjectThrow();
-        HandleObjectInteract();
+        //HandleObjectInteract();
+        rotate();
         HandleObjectRotation();
-        Debug.Log(Vector3.Distance(transform.position, holdPos.transform.position));
+        if (pickedUp) {
+        	Debug.Log(Vector3.Distance(transform.position, holdPos.transform.position));
+    	}
     }
 
 
     //Picks up the object from ground
-    private void HandleObjectInteract()
+    private void OnMouseOver()
     {
     	if (Input.GetKeyDown(KeyCode.E))
     	{
 	        if (Vector3.Distance(playerTransform.position, transform.position) < 2.2f && pickedUp == false)      //If player is close enough
 	        {
+
 	            //transform.SetParent(Camera.main.transform);                                       //sets the parent of the object to camera (object will be always in front of it)
-	            //rb.isKinematic = true;                                                      //turns Kinematic on, so object will not interfere when picked up
-	            transform.position = holdPos.transform.position;                      //sets local position in front of player, so it seems player is holding it
-	            transform.rotation = Quaternion.Euler(0, 0f, 0);                       //rotates the object
-	            rb.useGravity = false;
+	            //rb.isKinematic = true;           
+	            transform.position = holdPos.transform.position;                                           //turns Kinematic on, so object will not interfere when picked up
+	            joint =  gameObject.AddComponent<FixedJoint>();                       //sets local position in front of player, so it seems player is holding it
+	            joint.connectedBody = holdPos.GetComponent<Rigidbody>();
+                joint.enablePreprocessing = false;
+	            joint.axis = new Vector3(0,0,0);
+	            joint.enableCollision = false;
+	            joint.connectedAnchor = holdPos.transform.position;
+	            //transform.rotation = Quaternion.Euler(0, 0f, 0); 
+	            //holdPos.transform.rotation = Quaternion.Euler(0, 0f, 0);                       //rotates the object
+	            //rb.useGravity = false;
 	            pickedUp = true;                                                            //tell's that object was picked up
 	        }
 	      	else if (pickedUp == true)                            //if player clicked 'E' and picked up object before
        		{
-            	transform.SetParent(null);                                                  //unleashes object from its parent
-            	//rb.isKinematic = false;                                                     //turns kinematic off, so object can lightly fall on the ground
-            	rb.useGravity = true;
+            	Object.Destroy(joint);
             	pickedUp = false;                                                           //object is not picked up
 
         	}
 	    }
     }
-    private void FixedUpdate() 
+    private void rotate() 
     {
     	if (pickedUp == true)                            //if player clicked 'E' and picked up object before
        	{
        		//transform.localPosition = new Vector3(0f, 0f, 1.5f);
-       		rb.MovePosition(holdPos.transform.position); 
+       		//rb.MovePosition(holdPos.transform.position); 
        		//transform.position = Vector3.MoveTowards(transform.position, holdPos.transform.position, 4 * Time.deltaTime);
-       		transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0); 
+       		//holdPos.transform.localRotation = Quaternion.Euler(Camera.main.transform.eulerAngles.x, 0, 0); 
        	}
     }
     private void OnCollisionEnter(Collision collision)
@@ -73,28 +81,35 @@ public class PickupableObject : MonoBehaviour {
         if (collision.gameObject.tag == "Player" && pickedUp == true)
         {
             Physics.IgnoreCollision(plyr, GetComponent<Collider>());
-        }
+        } 
     }
-    private void OnCollisionStay(Collision CollisionData) // fuck unity physics, can't do decent collisions everything clips through with enough force.
+    private void OnCollisionExit(Collision collision) 
     {
-    	if (pickedUp == true)                            //if player clicked 'E' and picked up object before
-       	{
-    	    if (Vector3.Distance(transform.position, holdPos.transform.position) > 0.15) // so the dumbass thing doesn't clip through blocks
-    	    {
-    	    	
-            	rb.useGravity = true;
-            	pickedUp = false;  
-    	    }
+    	if (collision.gameObject.tag == "Player" && pickedUp == true)
+        {
+            Physics.IgnoreCollision(plyr, GetComponent<Collider>());
         }
     }
-    //Handles object throw
+    // private void avoidShake() // fuck unity physics, can't do decent collisions everything clips through with enough force.
+    // {
+    // 	if (pickedUp == true)                            //if player clicked 'E' and picked up object before
+    //    	{
+    // 	    if (Vector3.Distance(transform.position, holdPos.transform.position) > 2.2) // set to 0.15 so the dumbass thing doesn't clip through blocks
+    // 	    {
+    // 	    	rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+    // 	    } else {
+    //             rb.constraints = RigidbodyConstraints.None;
+    //         }
+    //     }
+    //}
+    
+        //Handles object throw
     void HandleObjectThrow()
     {
             if (Input.GetMouseButtonDown(0) && pickedUp == true)                        // throw on click
             {
 
-                transform.SetParent(null);                                              //unleashes object from its parent
-                rb.useGravity = true;                                              //turns off kinematic, so object can be physical again
+                Object.Destroy(joint);                                             //turns off kinematic, so object can be physical again
                 rb.AddForce(new Vector3(Camera.main.transform.forward.x,0.2f, Camera.main.transform.forward.z) * throwPower, ForceMode.Impulse);    //Adds force in direction where camera is looking with 'throwpower' power and Impulse forcemode
                 pickedUp = false;                                                       //object is not picked up
 
